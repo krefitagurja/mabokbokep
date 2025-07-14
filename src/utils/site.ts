9e7c727f935d976fd5e70c84099f937e0f310903
krefitagurja <krefitagurja@gmail.com>
2025-07-14 11:29:06 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Mabok Bokep';
-export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://mabokbokep.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
